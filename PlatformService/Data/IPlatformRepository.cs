dbf768ac4d6428a8e56ed471bf6978adeda5a226
Ivan Zakharov <ibzakharov2@asuproject.ru>
2023-08-08 23:27:31 +0500

    Add platform models

---
+++ PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,46 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepository
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform platform);
+}
+
+public class PlatformRepository : IPlatformRepository
+{
+    private readonly AppDbContext _context;
+
+    public PlatformRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() > 0;
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return _context.Platforms;
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        return _context.Platforms.FirstOrDefault(i => i.Id == id);
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        if (platform == null)
+        {
+            throw new ArgumentNullException(nameof(platform));
+        }
+
+        _context.Platforms.Add(platform);
+    }
+}
---
+++ PlatformService/Profiles/PlatformProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles;
+
+public class PlatformProfiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+} 
